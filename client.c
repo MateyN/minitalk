3a263e6e4b40788959bbd91ea4cb331a75704980
Matey Nikolov <mateynnikolov@gmail.com>
2021-12-16 16:03:26 +0100

    42 header and comments added

--- client.c
+++ client.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                             		        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnikolov <mnikolov@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/10 11:13:45 by mnikolov          #+#    #+#             */
+/*   Updated: 2021/12/16 14:06:07 by mnikolov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/libft.h"
 #include "minitalk.h"
 
@@ -14,11 +26,11 @@ void	send_char_by_bit(int pid, char *byte)
 		if ((byte[i] >> j) & 1)						// Iterate through the string shifting the bits by one to the right.
 		{
 			if (kill(pid, SIGUSR1) == -1)
-				write(1, "No server found\n", 16);	// Error if wrong PID is entered.
+				write(1, "No server found\n", 16);	// Error if wrong PID is entered for SIGUSR1.
 		}
 		else
 		{
-			if (kill(pid, SIGUSR2) == -1)
+			if (kill(pid, SIGUSR2) == -1)			// Error if wrong PID is entered for SIGUSR2.
 				write(1, "No server found\n", 16);
 		}
 		j++;
@@ -26,7 +38,7 @@ void	send_char_by_bit(int pid, char *byte)
 	}
 }
 
-static void	acknowledgment(int sig)
+static void	acknowledgment(int sig)					// Confirm every signal received by sending a signal to the client (ACK signal).
 
 {
 	static int	received = 0;
@@ -57,7 +69,7 @@ int	main(int argc, char **argv)
 		write(1, "Invalid arguments\n", 18);		// If the arguments are less than 3 display error.
 		return (1);
 	}
-	ft_putstr_fd("Message received!\n", 1);
+	ft_putstr_fd("Message received!\n", 1);			// Acknowledge sent.
 	signal(SIGUSR1, acknowledgment);
 	signal(SIGUSR2, acknowledgment);
 	handler((ft_atoi(argv[1])), argv[2]);
